38cdf607d11dad93f74a77a74d45ff6f3ef6b047
GruBaXu <108203631+LTGBLSN@users.noreply.github.com>
2026-01-12 19:37:12 +0800

    update

--- code_mf/Src/can_sent.c
+++ code_mf/Src/can_sent.c
@@ -59,19 +59,19 @@ void can_sent()
             }
             else if(rc_s0 == 3 | rc_s0 == 1)//运动模式
             {
-                can_cmd_all(0,
-                            0,
-                            0,
-                            0,
-                            0,
-                            PITCH_3510_ID5_GIVEN_CURRENT,
-                            FRICTION_WHEEL_3510_ID1_GIVEN_CURRENT,
-                            FRICTION_WHEEL_3510_ID2_GIVEN_CURRENT,
-                            FRICTION_WHEEL_3510_ID3_GIVEN_CURRENT,
-                            FRICTION_WHEEL_3510_ID4_GIVEN_CURRENT,
-                            FRICTION_WHEEL_3510_ID5_GIVEN_CURRENT,
-                            FRICTION_WHEEL_3510_ID6_GIVEN_CURRENT,
-                            SHOOT_3510_ID7_GIVEN_CURRENT);//PITCH_3510_ID5_GIVEN_CURRENT
+//                can_cmd_all(0,
+//                            0,
+//                            0,
+//                            0,
+//                            0,
+//                            PITCH_3510_ID5_GIVEN_CURRENT,
+//                            FRICTION_WHEEL_3510_ID1_GIVEN_CURRENT,
+//                            FRICTION_WHEEL_3510_ID2_GIVEN_CURRENT,
+//                            FRICTION_WHEEL_3510_ID3_GIVEN_CURRENT,
+//                            FRICTION_WHEEL_3510_ID4_GIVEN_CURRENT,
+//                            FRICTION_WHEEL_3510_ID5_GIVEN_CURRENT,
+//                            FRICTION_WHEEL_3510_ID6_GIVEN_CURRENT,
+//                            SHOOT_3510_ID7_GIVEN_CURRENT);//PITCH_3510_ID5_GIVEN_CURRENT
 
 //                can_cmd_all(CHASSIS_3508_ID1_GIVEN_CURRENT,
 //                            CHASSIS_3508_ID2_GIVEN_CURRENT,
